9e98d74ea86485b75ef4930646287dba1beaa89e
Steve Grubb <sgrubb@redhat.com>
2003-03-13 23:45:26 +0000

    sc_orig_bind_addr needs to be free'd on reconfig

--- xinetd/sconf.c
+++ xinetd/sconf.c
@@ -71,6 +71,7 @@ void sc_free( struct service_config *scp )
    COND_FREE( scp->sc_protocol.name ) ;
    COND_FREE( scp->sc_server ) ;
    COND_FREE( (char *)scp->sc_bind_addr ) ;
+   COND_FREE( (char *)scp->sc_orig_bind_addr ) ;
    if ( scp->sc_server_argv )
    {
       char **pp ;
